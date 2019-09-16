945e4a94b414f1ba7864db93b59c7366d6cf59b8
gongolin <mdelrosal@leasencloud.com>
2019-09-16 22:18:21 +0200

    programas

--- cursos/GOLANG/init.go
+++ cursos/GOLANG/init.go
@@ -7,3 +7,31 @@ import (
 func main() {
 	fmt.Println("Hola mundoooooo!")
 }
+
+// Leccion 8:  imagenes de GO:
+//declaracion o statement x=2+2
+//expresion  2+2
+//operando  2
+//opearador  +
+// keywords --> palabras claves
+//
+// Lección 11: Valor 0
+//Declarando la variable con el identificados z de tipo int:
+//var z int
+//Valor Cero
+//	Entendiendo el valor cero
+//		false para booleanos
+//		0 para integers
+//		0.0 para floats
+//		"" para strings
+//		nil para
+//			pointers
+//			funciones
+//			interfaces
+//			slices
+//			channels
+//			maps
+//Usar el operador de declaración corta de variables mientras más sea posible
+//Usar var para
+//	Valor cero
+//	package scope
