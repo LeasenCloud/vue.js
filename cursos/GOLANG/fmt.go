b32e4cfdce93515e6767f632535660be84d365a2
gongolin <mdelrosal@leasencloud.com>
2019-09-17 21:24:36 +0200

    añadir

---
+++ cursos/GOLANG/fmt.go
@@ -0,0 +1,6 @@
+// Leccion 12. El paquete fmt (Println, Printf, Print )
+//f := "dice hola mundo"  --> string literal interpretado.- No podemos tener linea nuevas
+//g := `El doctor dice que comer vegetales es:
+//"saludable"`   -->  row striog literal.- Podemos tener lineas nuevas, espacios , \, ' etc
+// Acceso directo a documentacion de paquetes:
+// https://godoc.org/fmt
