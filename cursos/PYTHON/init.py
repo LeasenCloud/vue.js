3f1dca85a6862e0afdd4bbcf4f1246d94d19c6cf
gongolin <mdelrosal@leasencloud.com>
2019-09-19 22:13:14 +0200

    añadir

--- cursos/PYTHON/init.py
+++ cursos/PYTHON/init.py
@@ -1,2 +1,26 @@
+# Python prmite el tipado dinamico
+...
+El tipado dinamico es que una misma variable puede
+contener diferentes tipos de datos a lo largo
+del programa
+...
 print("Hola mundo\n")
 print("hola la")
+
+# operadores aritmeticos
+# Division entera:
+num1 = 10
+num2 = 3
+resultado = num1 / num2
+print(resultado)  # Nos dara 3.3333333
+resultado = num1 // num2
+print(resultado)  # Nos dara 3
+resultado = num1 % num2
+print(resultado)  # Nos dara 1. Nos da el resto de la division
+resultado = num1 ** num2
+print(resultado)  # Nos dara 35 es decir 2 elevado a 5
+# (exponencial o elevado a una potencia)
+# La prioridad de los operadores aritmeticos es:  (), **, *, /, %, +, -
+
+# operadores relacionales. Se ejecutan despues de los aritmeticos
+# < > <= >= != ==
